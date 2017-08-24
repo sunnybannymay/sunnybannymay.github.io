4c0eadb64f104278d21032d64d83f36f7ac1fd01
Мясное котясо <nadya.kott@gmail.com>
2017-08-24 19:57:36 +0500

    site final

---
+++ config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const pkg = require('../package');
+
+module.exports = {
+  port: 4000,
+  title: 'summer-school-2017',
+  // when you use electron please set to relative path like ./
+  // otherwise only set to absolute path when you're using history mode
+  publicPath: '/',
+  cssModules: true,
+  jsx: true
+};
